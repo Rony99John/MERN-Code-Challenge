5067830757fd0d97974dbb9eae43f0fb13e9b524
Rony <ronyjohn9870@gmail.com>
2022-12-03 11:05:30 +0200

    added routes

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const CategoriesModel = require("./models/Categories");
 
 const cors = require("cors");
 
@@ -12,33 +11,9 @@ mongoose.connect(
 app.use(express.json());
 app.use(cors());
 
-app.post("/createCategory", async (req, res) => {
-  try {
-    const category = {
-      name: "Platters",
-      items: [
-        {
-          name: "Chicken Platter",
-          description:
-            "4 chicken pieces with our special sauce served with bbq dip and wedges",
-          price: 160000,
-          image: "https://via.placeholder.com/150",
-        },
-      ],
-    };
-    const newCategory = new CategoriesModel(category);
-    await newCategory.save();
+const routes = require("./routes/routes");
 
-    res.send(category);
-  } catch (err) {
-    console.log("hello");
-    console.log(err);
-  }
-});
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use("/", routes);
 
 app.listen(3001, () => {
   console.log("Server runnning on port 3001");
---
+++ server/routes/routes.js
@@ -0,0 +1,39 @@
+const router = require("express").Router();
+
+const CategoriesModel = require("../models/Categories");
+
+router.post("/createCategory", async (req, res) => {
+  try {
+    const category = {
+      name: "Platters",
+      items: [
+        {
+          name: "Chicken Platter",
+          description:
+            "4 chicken pieces with our special sauce served with bbq dip and wedges",
+          price: 160000,
+          image: "https://via.placeholder.com/150",
+        },
+      ],
+    };
+    const newCategory = new CategoriesModel(category);
+    await newCategory.save();
+
+    res.send(category);
+  } catch (err) {
+    console.log("hello");
+    console.log(err);
+  }
+});
+
+router.get("/getCategories", (req, res) => {
+  CategoriesModel.find({}, (err, result) => {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json(result);
+    }
+  });
+});
+
+module.exports = router;
